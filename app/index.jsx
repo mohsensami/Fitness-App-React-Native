a6f63157d64cb3c73cd73e74916fbc4337df67de
mohsensami <mohsensami88@gmail.com>
2023-12-30 23:11:54 +0330

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,36 @@
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
+
+export default function index() {
+    return (
+        <View className="flex flex-1  justify-end ">
+            <StatusBar style="dark" />
+            <Image className="h-full w-full absolute" source={require('../assets/images/welcome.png')} />
+            <LinearGradient
+                colors={['transparent', '#18181b']}
+                style={{ width: wp(100), height: hp(70) }}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 0.8 }}
+                className="flex justify-end pb-12 space-y-8"
+            >
+                <Text style={{ fontSize: hp(5) }} className="text-white font-bold tracking-wide">
+                    Best <Text className="text-rose-500">Workouts</Text>
+                </Text>
+                <Text style={{ fontSize: hp(5) }} className="text-white font-bold tracking-wide">
+                    For You
+                </Text>
+                <TouchableOpacity
+                    onPress={() => router.push('home')}
+                    style={{ height: hp(7), width: wp(80) }}
+                    className="bg-rose-500 flex items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+                >
+                    <Text style={{ fontSize: hp(3) }} className="text-white font-bold tracking-widest">
+                        Get Started
+                    </Text>
+                </TouchableOpacity>
+            </LinearGradient>
+        </View>
+    );
+}
