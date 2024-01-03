da44a261f5ceeafe812031e0fffb42e2253ba6cf
mohsensami <mohsensami88@gmail.com>
2024-01-03 20:57:51 +0330

    ExcerciseDetails

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,18 @@
 import { View, Text, LogBox } from 'react-native';
 import React from 'react';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function _layout() {
     LogBox.ignoreLogs(['Warning: Failed prop type']);
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-            }}
-        />
+        <>
+            <StatusBar style="dark" />
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                }}
+            />
+        </>
     );
 }
